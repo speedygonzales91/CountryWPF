e91cb37cf5d7fff4a4b695e47cbff690b6d7dce0
Zsolt Molnár <mozb17@gmail.com>
2019-11-08 16:59:12 +0100

    Add Api class. Add JsonConverter Nuget.

---
+++ ViewModel/RestCountryApi.cs
@@ -0,0 +1,29 @@
+﻿using CountryWPF.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryWPF.ViewModel
+{
+    public class RestCountryApi
+    {
+        public const string BASE_URL = "https://restcountries.eu/rest/v2/all";
+
+        public async static Task<List<Country>> GetCountries()
+        {
+            List<Country> countries = new List<Country>();
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(BASE_URL);
+                var json = await response.Content.ReadAsStringAsync();
+                countries = JsonConvert.DeserializeObject<List<Country>>(json);
+            }
+
+            return countries;
+        }
+    }
+}
